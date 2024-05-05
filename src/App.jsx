e4f0c3980314a5cbbe8d27d2629530a611f7e3e6
Sanjoy Sarkar <sanjoy8794@gmai.com>
2024-05-06 00:58:11 +0530

    fetch:navorfootter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-
+// App.js
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import About from './About';
--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,31 @@
 // Layout.js
+import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import LargeWithAppLinksAndSocial from './Footer';
+import WithSubnavigation from './Navbar';
 function Layout() {
     return (
-        <div>
-            <header>
-                <h1>My App</h1>
-                <nav>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <Outlet />
-            </main>
-        </div>
+        <ChakraProvider>
+            <WithSubnavigation />
+            <div>
+                <header>
+                    <h1>My App</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+            <LargeWithAppLinksAndSocial />
+        </ChakraProvider>
+
     );
 }
 
